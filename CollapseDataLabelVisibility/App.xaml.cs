2d1461f0e4bd25585c2d47c49b86562aee1cfe38
EzhilarasanElangovan31 <ezhilarasan.elangovan@syncfusion.com>
2024-04-26 18:01:26 +0530

    Updated Sample.

---
+++ CollapseDataLabelVisibility/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollapseDataLabelVisibility
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CollapseDataLabelVisibility/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace CollapseDataLabelVisibility
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+
+    public class VisibilityConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (!(value is double))
+                return null;
+
+            var labelValue = (double)value;
+            if (labelValue < 50)
+                return false;
+            return true;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+}
